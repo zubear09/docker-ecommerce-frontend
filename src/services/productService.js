b682631360ab35465f8d19e74f19da926bc94b8e
zubear09 <zubearahammad786@gmail.com>
2025-09-20 09:51:42 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http:54.221.163.250:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
@@ -17,3 +17,4 @@ export const getProducts = async (category = "") => {
 export const getProductImageUrl = (imagePath) => {
   return `http://localhost:8080/api/products/images/${imagePath}`;
 };
+
